1b07b369d07d8ba3f1b5324fda22a644290cf351
Tyler McGinnis <tylermcginnis@gmail.com>
2025-12-02 08:55:56 -0500

    Add email and password authentication

--- test/controllers/tables_controller_test.rb
+++ test/controllers/tables_controller_test.rb
@@ -1,6 +1,10 @@
 require "test_helper"
 
 class TablesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    sign_in_as users(:one)
+  end
+
   test "index" do
     # TODO: Use helper once model created
     get "/"
--- test/system/managing_tables_test.rb
+++ test/system/managing_tables_test.rb
@@ -1,9 +1,11 @@
 require "application_system_test_case"
 
 class ManagingTablesTest < ApplicationSystemTestCase
-  test "managing tables" do
-    # TODO: Use helper once model created
-    visit root_url
+  setup do
+    sign_in_as users(:one).email_address
     assert_selector "h1", text: "Tables"
+  end
+
+  test "managing tables" do
   end
 end
