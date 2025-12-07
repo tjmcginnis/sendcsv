8f2192b281c7c8c2de00b7111c53d82f8f909148
Tyler McGinnis <tylermcginnis@gmail.com>
2025-12-07 16:39:46 -0500

    Add copy to clipboard for ingestion url

--- test/system/smoke_test.rb
+++ test/system/smoke_test.rb
@@ -1,18 +1,20 @@
 require "application_system_test_case"
 
-class ManagingTablesTest < ApplicationSystemTestCase
-  setup do
+class SmokeTest < ApplicationSystemTestCase
+  test "navigating tables" do
     sign_in_as users(:one).email_address
     assert_selector "h1", text: "Tables"
-  end
 
-  test "managing tables" do
     assert_selector "p", text: tables(:one).name
     assert_selector "p", text: tables(:one).public_id
 
     click_on "View table"
 
-    assert_selector "h1", text: tables(:one).name
+    assert_selector "h1", text: tables(:one).name, wait: 5
+
+    assert_field "Ingestion URL", with: in_url(tables(:one).public_id)
+    assert_selector "button", text: "Copy to clipboard"
+
     assert_selector "th", text: "Name"
     assert_selector "th", text: "Age"
     assert_selector "th", text: "City"
